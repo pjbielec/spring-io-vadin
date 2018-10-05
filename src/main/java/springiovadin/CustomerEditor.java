4a8024ce56c266d5ed5059d200679f6af51d458a
pjbielec <przemyslaw.bielec@core3.pl>
2018-10-05 15:33:45 +0200

    e -> save() not working

---
+++ src/main/java/springiovadin/CustomerEditor.java
@@ -0,0 +1,70 @@
+package springiovadin;
+
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.KeyNotifier;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.binder.Binder;
+import com.vaadin.flow.spring.annotation.SpringComponent;
+import com.vaadin.flow.spring.annotation.UIScope;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.swing.plaf.basic.BasicMenuUI;
+
+/**
+ * A simple example to introduce building forms. As your real application is probably much
+ * more complicated than this example, you could re-use this form in multiple places. This
+ * example component is only used in MainView.
+ * <p>
+ * In a real world application you'll most likely using a common super class for all your
+ * forms - less code, better UX.
+ */
+
+@SpringComponent
+@UIScope
+public class CustomerEditor extends VerticalLayout implements KeyNotifier {
+
+    private final CustomerRepository repository;
+
+    /**
+     * The currently edited customer
+     */
+    private Customer customer;
+
+    /* Fields to edit properties in Customer entity */
+    TextField firstName = new TextField("First name");
+    TextField lastName = new TextField("Last name");
+
+    /* Action buttons */
+    Button save = new Button("Save", VaadinIcon.CHECK.create());
+    Button cancel = new Button("Cancel");
+    Button delete = new Button("Delete", VaadinIcon.TRASH.create());
+    HorizontalLayout actions = new HorizontalLayout(save, cancel, delete);
+
+    Binder<Customer> binder = new Binder<>(Customer.class);
+    private BasicMenuUI.ChangeHandler changeHandler;
+
+    @Autowired
+    public CustomerEditor(CustomerRepository repository){
+        this.repository = repository;
+
+        add(firstName, lastName, actions);
+
+        //bind using naming convention
+        binder.bindInstanceFields(this);
+
+        //Configure and style components
+        setSpacing(true);
+
+        save.getElement().getThemeList().add("primary");
+        delete.getElement().getThemeList().add("error");
+
+        addKeyPressListener(Key.ENTER, e -> save());
+    }
+
+
+
+}
